e48b2b7c9dc28a2731a7c18ac6f6c740b74148c7
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-09-26 23:51:43 +0000

    update13

--- includes/functions.php
+++ includes/functions.php
@@ -52,12 +52,12 @@ function page_title()
 function page_content()
 {
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-    $path = getcwd() . '/' . config('content_path') . '/' . $page . '.php';
+    $path = getcwd() . '/' . config('content_path') . '/' . $page . '.phtml';
     if (! file_exists($path)) {
-        $path = getcwd() . '/' . config('content_path') . '/404.php';
+        $path = getcwd() . '/' . config('content_path') . '/404.phtml';
     }
-    //echo file_get_contents($path);
-    require config('content_path'). '/'. $page.'.php';
+    echo file_get_contents($path);
+    //require config('content_path'). '/'. $page.'.php';
 }
 /**
  * Starts everything and displays the template.
