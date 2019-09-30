87a876696cf4333ade05de0271915bebf6594e62
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-09-30 19:43:42 +0000

    s

--- content/random.php
+++ content/random.php
@@ -8,7 +8,6 @@
         <br>
         <h3>2017</h3>
         <br>
-        <img style="width:100%; height: auto;" src="http://xkcd.com/614/info.0.json" alt="test">
         <?php getComic(); ?>
         </div>
         </div>
