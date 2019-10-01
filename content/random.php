d02d955b5bfebf972eb04cf5bc48a6c965dbf15c
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-01 01:30:15 +0000

    dsf

--- content/random.php
+++ content/random.php
@@ -1,18 +1,10 @@
-<div class="row" style="padding-top:15px; margin-right: 0px; margin-left: 0px;">
-  <button id="testButton" type="button" class="mx-auto btn btn-dark">Generate Random Comic</button>
-</div>
-<div class="row" style="margin-right: 0px; margin-left: 0px;">
-	<div class="mx-auto" style="padding:20px">
-    <img style="width:auto; height: auto;" src = <?php getRandom(); ?>>
-    </div>
-    </div>
 <script>
-    $('#testButton').click(function(e)){
+    function comic(){
     $.ajax(
     {
-        type: 'post',  //whats your request type
-        url: "/content/random.php",  // whats your php file
-        data: {'random' : true}, //what data are you sending via JSON
+        type: 'GET',  //whats your request type
+        url: "file:///workspace/Assignment2/includes/functions.php",  // whats your php file
+        data: {'link' : ""}, //what data are you sending via JSON
         dataType:"HTML", // what type of data are you getting back
         success: function(data)
         {
@@ -23,6 +15,15 @@
             alert("failed to get data");//what to do if it fails
         }
 }); // Ajax close
-return false;
-})
+
+}
 </script>
+<div class="row" style="padding-top:15px; margin-right: 0px; margin-left: 0px;">
+  <button id="testButton" type="button" class="mx-auto btn btn-dark">Generate Random Comic</button>
+</div>
+<div class="row" style="margin-right: 0px; margin-left: 0px;">
+	<div class="mx-auto" style="padding:20px">
+    <img style="width:auto; height: auto;" src = <?php getRandom(); ?>>
+    </div>
+    </div>
+    <button onClick="comic()" >Generate Random Comic</button>
