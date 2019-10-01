62161250b9746728107f5b1cf7a048460f03e5af
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-01 00:33:43 +0000

    rter

--- template/template.php
+++ template/template.php
@@ -8,27 +8,27 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title><?php page_title(); ?></title>
+    <title><?php site_name(); ?></title>
   </head>
   <body style="padding-bottom: 75px;">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-     <a class="navbar-toggler navbar-brand" href="http://csu-assignment2.herokuapp.com">NavBar</a>
+     <a class="navbar-toggler navbar-brand" href="#">NavBar</a>
      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
      </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a><?php nav_menu(); ?></a>
-    </div>
+    <ul class="navbar-nav">
+      <?php nav_menu(); ?>
+    </ul>
   </div>
 </nav>
+<?php page_content(); ?>
     <div class="jumbotron">
     <h1>Comic Generator!</h1>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<?php page_content(); ?>
 </body>
 </html>
