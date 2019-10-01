0dca23de2f6bcc4b164d7345e2d950c5e2ad421d
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-01 02:10:14 +0000

    dsiufh

--- content/random.php
+++ content/random.php
@@ -1,22 +1,21 @@
-<script>
-    function Ccomic(){
-    $.ajax(
-    {
-        type: 'GET',  //whats your request type
-        url: "file:///workspace/Assignment2/includes/functions.php",  // whats your php file
-        data: {'link' : ""}, //what data are you sending via JSON
-        dataType:"html", // what type of data are you getting back
-        success: function(data)
-        {
-            alert(data)//what are you going to do with what you get
-        },
-        error: function(result)
-        {
-            alert("failed to get data");//what to do if it fails
-        }
-}); // Ajax close
+<script >
+            function ChangeComic(){
+                 //location.reload();
+                $.ajax({
+                    type: "GET",
+                    url: "file:///workspace/Assignment2/includes/functions.php",
+                    data: {'link': ""},
+                    dataType: "html",
+                    success: function (data) {
+                        alert(data);
+                    },
+                    error: function (result) {
+                        alert("Error");
+                    }
+                });
+            }
+        </script>
 
-}
-</script>
-<button onclick="Ccomic()" class="mx-auto btn btn-dark">Generate Random Comic</button>
-<div><img style="width:auto; height: auto;" src = <?php imageRand(); ?>></div>
+
+<div class="d-flex justify-content-center"><img src = <?php imageRand(); ?>></div>
+<button onClick="ChangeComic()" >New Comic</button>
