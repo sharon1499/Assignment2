81ca7ee6b213f06835467ed16f072317aef1ef1c
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-01 03:42:09 +0000

    upsjd

--- content/random.php
+++ content/random.php
@@ -1,5 +1,5 @@
 <script>
-    $('#testButton').click(function(e)){
+    $('#testButton').on('click',function(e)){
     $.ajax(
     {
         type: 'GET',  //whats your request type
@@ -8,15 +8,15 @@
         dataType:"json", // what type of data are you getting back
         success: function(data)
         {
-            $('div #target_div').html(data)//what are you going to do with what you get
+            console.log('success', data);
+            //$('div #target_div').html(data)//what are you going to do with what you get
         },
         error: function(result)
         {
             alert("failed to get data");//what to do if it fails
         }
 }); // Ajax close
-return false;
-}
+});
 </script>
 <div class="row" style="padding-top:15px; margin-right: 0px; margin-left: 0px;">
   <button id="testButton" type="button" class="mx-auto btn btn-dark" >Generate Random Comic</button>
