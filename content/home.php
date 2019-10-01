7d3135025253d02fa701ab0b9b3df16b59408172
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-01 02:45:11 +0000

    aojedff

--- content/home.php
+++ content/home.php
@@ -1,3 +1,6 @@
+<div class="jumbotron">
+    <h1>Comic Generator!</h1>
+    </div>
 <div class="row" style="margin-right: 0px; margin-left: 0px;">
 	<div class="mx-auto" style="padding:20px">
     <div> <?php getTitle(); ?> </div>
--- template/template.php
+++ template/template.php
@@ -10,7 +10,7 @@
 
     <title><?php site_name(); ?></title>
   </head>
-  <body style="padding-bottom: 75px;">
+  <body style="padding-bottom: 100px;">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
      <a class="navbar-toggler navbar-brand" href="#">NavBar</a>
@@ -23,9 +23,6 @@
     </ul>
   </div>
 </nav>
-    <div class="jumbotron">
-    <h1>Comic Generator!</h1>
-    </div>
     <?php page_content(); ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
