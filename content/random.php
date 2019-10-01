3d3ed2c758d023c79f8082c6948f24a1e0f1dcd9
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-01 02:03:09 +0000

    saiu

--- content/random.php
+++ content/random.php
@@ -2,10 +2,10 @@
     function comic(){
     $.ajax(
     {
-        type: 'GET',  //whats your request type
+        type: 'get',  //whats your request type
         url: "file:///workspace/Assignment2/includes/functions.php",  // whats your php file
         data: {'link' : ""}, //what data are you sending via JSON
-        dataType:"HTML", // what type of data are you getting back
+        dataType:"html", // what type of data are you getting back
         success: function(data)
         {
             alert(data)//what are you going to do with what you get
@@ -18,11 +18,6 @@
 
 }
 </script>
-<div class="row" style="margin-right: 0px; margin-left: 0px;">
-	<div class="mx-auto" style="padding:20px">
-    <img style="width:auto; height: auto;" src = <?php imageRand(); ?>>
-    </div>
-    </div>
-    <div class="row" style="padding-top:15px; margin-right: 0px; margin-left: 0px;">
-  <button id="testButton" type="button" onclick="comic()" class="mx-auto btn btn-dark">Generate Random Comic</button>
-</div>
+
+<div><img style="width:auto; height: auto;" src = <?php imageRand(); ?>></div>
+<button onclick="comic()" class="mx-auto btn btn-dark">Generate Random Comic</button>
