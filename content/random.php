464d812c3038ff0348c0a1b4b926c0e1c1f72875
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-06 02:25:05 +0000

    button77

--- content/random.php
+++ content/random.php
@@ -1,25 +1,5 @@
-<script>
-    $('#testButton').click(function(){
-    $.ajax(
-    {
-        type: 'POST',  //whats your request type
-        url: "..includes/functions.php",  // whats your php file
-        data: {"random" : true}, //what data are you sending via JSON
-        dataType:"HTML", // what type of data are you getting back
-        success: function(result)
-        {
-            $('div #target_div').html(result)
-            //$('div #target_div').html(data)//what are you going to do with what you get
-        },
-        error: function(result)
-        {
-            alert("failed to get data");//what to do if it fails
-        }
-}); // Ajax close
-});
-</script>
 <div class="row" style="padding-top:15px; margin-right: 0px; margin-left: 0px;">
-  <button id="testButton" name = "submit" type="button" class="mx-auto btn btn-dark" >Generate Random Comic</button>
+  <button id="button" class="mx-auto btn btn-dark" >Generate Random Comic</button>
 </div>
 <div class="row" style="margin-right: 0px; margin-left: 0px;">
   <div class="mx-auto" style="padding:20px" id="target_div">
--- template/template.php
+++ template/template.php
@@ -27,5 +27,24 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
+<script>
+    $('#button').click(function(){
+    $.ajax(
+    {
+        type: 'POST',  //whats your request type
+        url: "..includes/functions.php",  // whats your php file
+        data: {"random" : true}, //what data are you sending via JSON
+        dataType:"HTML", // what type of data are you getting back
+        success: function(result)
+        {
+            $("#target_div").html(result)
+            //$('div #target_div').html(data)//what are you going to do with what you get
+        },
+        error: function(result)
+        {
+            alert("failed to get data");//what to do if it fails
+        }
+    })
+})
+    </body>
 </html>
