00fe3e38df5902007c56bdf54a100711e561db6b
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-27 17:13:51 +0000

    fd

--- content/home.php
+++ content/home.php
@@ -3,7 +3,6 @@
     </div>
 <div class="row" style="margin-right: 0px; margin-left: 0px;">
 	<div class="mx-auto" style="padding:20px">
-    <div> <?php getTitle(); ?> </div>
     <img style="width:auto; height: auto;" src = <?php getComicImage(); ?>>
     </div>
     </div>
