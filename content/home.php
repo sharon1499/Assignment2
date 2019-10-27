928eb17c3a8cb4dff0e158d3baef7e94f3d145ee
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-10-27 17:21:46 +0000

    sduh

--- content/home.php
+++ content/home.php
@@ -1,8 +1,2 @@
-<div class="jumbotron">
-    <h1>Comic Generator!</h1>
-    </div>
-<div class="row" style="margin-right: 0px; margin-left: 0px;">
-	<div class="mx-auto" style="padding:20px">
-    <img style="width:auto; height: auto;" src = <?php getComicImage(); ?>>
-    </div>
+ <div><?php getComicImage(); ?>>
     </div>
