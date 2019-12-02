5771d0f509ed34c0b5e2e3a8ff688d23fac79257
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-12-02 17:24:06 +0000

    fixing button

--- content/random.php
+++ content/random.php
@@ -18,7 +18,7 @@
             });
            });
         </script>
-<button id="change" class="btn btn-info" style="padding:50px; display:block; margin:auto; width:50%; height:20px; font-size:20;">Generate Comic</button></div>
+<button id="change" class="btn btn-info" style="padding:50px; display:block; margin:auto; width:50%; height:5px; font-size:50;">Generate Comic</button></div>
 <div id="randomComic" class="padding-image">
 <div> <?php getComicRand(); ?> </div>
 </div>
