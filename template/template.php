7dc5ebeb1d4d56db045b707022d138f9c35ee1e6
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-12-02 17:56:51 +0000

    fixing button

--- template/template.php
+++ template/template.php
@@ -12,6 +12,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="/template/style.css" rel="stylesheet" type="text/css" />
 <div class="jumbotron"> <h1 class="d-flex justify-content-center">Welcome To The Comic Generator!</h1> </div>
     <title><?php site_name(); ?></title>
   </head>
